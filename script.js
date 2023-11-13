9fa3c564ec6c06b0b4c10ff96d88292844ae72db
santiagobolado <sbolado@itba.edu.ar>
2023-11-13 15:29:25 -0300

    a

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
+// Array que representa los productos en el carrito
+let productosEnCarrito = [];
+
 // Función para agregar un producto al carrito
-function agregarAlCarrito(idProducto) {
-    // Buscar el producto en el carrito por su ID
-    let producto = productosEnCarrito.find(function(item) {
-      return item.id === idProducto;
-    });
-  
-    // Si el producto no existe en el carrito, agregarlo con cantidad 1
-    if (!producto) {
-      productosEnCarrito.push({ id: idProducto, cantidad: 1 });
-    } else {
-      // Si el producto ya existe, incrementar la cantidad
-      producto.cantidad++;
-    }
-  
-    // Actualizar el carrito en la página
-    actualizarCarrito();
-  }
-  
+function agregarAlCarrito() {
+    // Simulación de agregar un producto al carrito
+    //let nuevoProducto = "Producto añadido";
+    //productosEnCarrito.push(nuevoProducto);
+
+    // Mostrar un mensaje al usuario
+    alert("Producto añadido al carrito");
+
+    // También podrías realizar otras acciones relacionadas con agregar al carrito aquí
+}
+
+// Ejemplo de cómo podrías llamar a la función desde otro lugar o en respuesta a un evento
+agregarAlCarrito();
+
